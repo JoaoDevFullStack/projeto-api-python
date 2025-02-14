f73bfb2f46e90d605dea64798a75faea3a6d518f
João Vitor <joaovvsantos77@gmail.com>
2025-02-14 04:12:57 -0300

    cliente.py

--- cliente-python/cliente.py
+++ cliente-python/cliente.py
@@ -1,26 +1,25 @@
 import requests
 
-# Função para buscar um registro pelo ID
+
 def buscar_registro(id):
-    url = f'http://localhost:5200/api/pessoa/{id}'  # URL da sua API C#
+    url = f'http://localhost:5200/api/pessoa/{id}'  
     
     try:
-        response = requests.get(url)  # Requisição GET para buscar o registro
+        response = requests.get(url)
         
         if response.status_code == 200:
-            pessoa = response.json()  # Converte a resposta para formato JSON
-            categorizar_idade(pessoa)  # Chama a função para categorizar a idade
+            pessoa = response.json() 
+            categorizar_idade(pessoa) 
         else:
             print(f"Erro ao buscar o registro. Código de status: {response.status_code}")
     except requests.exceptions.RequestException as e:
         print(f"Ocorreu um erro na requisição: {e}")
 
-# Função para categorizar a idade
+
 def categorizar_idade(pessoa):
     idade = pessoa['idade']
     categoria = ""
     
-    # Categorizar a pessoa com base na idade
     if idade < 30:
         categoria = "Jovem"
     elif 30 <= idade <= 40:
@@ -28,13 +27,12 @@ def categorizar_idade(pessoa):
     else:
         categoria = "Sênior"
     
-    # Exibir as informações organizadas
     print(f"Nome: {pessoa['nome']}")
     print(f"Idade: {pessoa['idade']} ({categoria})")
     print(f"Cidade: {pessoa['cidade']}")
     print(f"Profissão: {pessoa['profissao']}")
     print("\n")
 
-# Exemplo de como usar a função
+
 id_pessoa = int(input("Digite o ID da pessoa: "))
 buscar_registro(id_pessoa)
